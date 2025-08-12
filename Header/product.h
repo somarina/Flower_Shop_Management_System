8f5d4ae5a97fad2905aa4c90127c1830f6603ce5
unknown <somarinak@gmail.com>
2025-08-12 16:13:05 +0700

    Update user.h: fixed login validation

--- Header/product.h
+++ Header/product.h
@@ -1,6 +1,6 @@
 /*        Noted For this Header File
 
-    This header file [product.h] use for store
+      This header file [product.h] use for store
         - class product that have inheritance from class Data in 
           header file data.h
         - use for take setter and getter from class Data
@@ -13,6 +13,7 @@
 
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 #include "productData.h"
+#include "ProductDesign.h"
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
@@ -25,6 +26,8 @@ public:
     void DisplayFlowerData();
     void EditFlowerData();
     static void Header();
+    static void DesignUpdateInsert();
+    static void DesignSearchDisplay();
 };
 
 // Function to generate next Flower ID
@@ -46,7 +49,77 @@ int GenerateNextFlowerID()
     file.close();
     return maxID + 1;
 }
+//design for  update
+void Product::DesignUpdateInsert(){
+	H::VLine(42,12,15,230,219);
+	H::VLine(71,12,15,230,219);
+	
+	//HLIne for insert 
+	H::HLine(37,14,38,196,220);
+	H::HLine(37,16,38,196,220);
+	H::HLine(37,18,38,196,220);
+	H::HLine(37,20,38,196,220);
+	H::HLine(37,22,38,196,220);
+	H::HLine(37,24,38,196,220);
+	H::HLine(37,26,38,196,220);
 
+	H::VLine(95,12,15,230,219);
+	H::VLine(129,12,15,230,219);
+	
+	//HLIne for insert 
+	H::HLine(90,14,43,196,220);
+	H::HLine(90,16,43,196,220);
+	H::HLine(90,18,43,196,220);
+	H::HLine(90,20,43,196,220);
+	H::HLine(90,22,43,196,220);
+	H::HLine(90,24,43,196,220);
+	H::HLine(90,26,43,196,220);
+	
+H::setcolor(6);H::gotoxy(12,16);cout<<R"(      .--.)" ;  
+H::setcolor(6);H::gotoxy(12,17);cout<<R"(    .'_\/_'.)" ;  
+H::setcolor(6);H::gotoxy(12,18);cout<<R"(    '. /\ .')" ;  
+H::setcolor(2);H::gotoxy(12,19);cout<<R"(      "||")" ;  
+H::setcolor(2);H::gotoxy(12,20);cout<<R"(       || /\)" ;  
+H::setcolor(2);H::gotoxy(12,21);cout<<R"(    /\ ||//\))" ;  
+H::setcolor(2);H::gotoxy(12,22);cout<<R"(   (/\\||/)" ;  
+H::setcolor(2);H::gotoxy(12,23);cout<<R"(______\||/_______)" ;  
+H::setcolor(2);H::gotoxy(12,24);cout<<R"(^^^^^^^^^^^^^^^^^)" ; 
+
+H::setcolor(1);H::gotoxy(141,16);cout<<R"(      .--.)" ;  
+H::setcolor(1);H::gotoxy(141,17);cout<<R"(    .'_\/_'.)" ;  
+H::setcolor(1);H::gotoxy(141,18);cout<<R"(    '. /\ .')" ;  
+H::setcolor(2);H::gotoxy(141,19);cout<<R"(      "||")" ;  
+H::setcolor(2);H::gotoxy(141,20);cout<<R"(       || /\)" ;  
+H::setcolor(2);H::gotoxy(141,21);cout<<R"(    /\ ||//\))" ;  
+H::setcolor(2);H::gotoxy(141,22);cout<<R"(   (/\\||/)" ;  
+H::setcolor(2);H::gotoxy(141,23);cout<<R"(______\||/_______)" ;  
+H::setcolor(2);H::gotoxy(141,24);cout<<R"(^^^^^^^^^^^^^^^^^)" ; 
+ 
+
+	
+}
+//design searchdisplay
+void Product::DesignSearchDisplay(){
+	
+	H::VLine(10,6,25,3,179);
+	H::VLine(161,6,25,3,179);
+	H::setcolor(7);H::gotoxy(9,14);cout<<char(70);
+	H::setcolor(7);H::gotoxy(9,16);cout<<char(76);
+	H::setcolor(7);H::gotoxy(9,18);cout<<char(79);
+	H::setcolor(7);H::gotoxy(9,20);cout<<char(87);
+	H::setcolor(7);H::gotoxy(9,22);cout<<char(69);
+	H::setcolor(7);H::gotoxy(9,24);cout<<char(82);
+	
+	H::setcolor(7);H::gotoxy(160,14);cout<<char(70);
+	H::setcolor(7);H::gotoxy(160,16);cout<<char(76);
+	H::setcolor(7);H::gotoxy(160,18);cout<<char(79);
+	H::setcolor(7);H::gotoxy(160,20);cout<<char(87);
+	H::setcolor(7);H::gotoxy(160,22);cout<<char(69);
+	H::setcolor(7);H::gotoxy(160,24);cout<<char(82);
+	
+	H::drawBoxDoubleLineWithBG(20,14,130,1,3);
+	H::drawBoxDoubleLineWithBG(20,17,130,8,4);
+}
 // Implementation of Product methods
 void Product::InputFlowerData()
 {
@@ -56,68 +129,86 @@ void Product::InputFlowerData()
     string strQty, strPrice;
 
     int id = GenerateNextFlowerID(); // auto ID
-    SetFlowerId(id);
+    SetFlowerID(id);
+    H::setcolor(199);H::gotoxy(69,16);cout <<"Enter Flower ID ";          
+    H::setcolor(199);H::gotoxy(69,18);cout <<"Enter Flower Name";         
+    H::setcolor(199);H::gotoxy(69,20);cout <<"Enter Flower Type";         
+    H::setcolor(199);H::gotoxy(69,22);cout <<"Enter Flower Price";        
+    H::setcolor(199);H::gotoxy(69,24);cout <<"Enter Flower Quantity";     
+    H::setcolor(199);H::gotoxy(69,26);cout <<"Enter Flower Description";  
+    H::setcolor(199);H::gotoxy(69,28);cout <<"Enter Flower Date";         
 
-    cout << "\n\tFlower ID                : " << GetFlowerId();
-    cout << "\n\tEdit Flower Name         : "; H::inputLetter(name, 15);
-    cout << "\n\tEdit Flower Type         : "; H::inputLetter(type, 15);
-    cout << "\n\tEdit Flower Price        : "; price = stod(H::inputUNumber(strPrice, 6));
-    cout << "\n\tEdit Flower Quantity     : "; qty = stoi(H::inputUNumber(strQty, 4));
-    cout << "\n\tEdit Flower Description  : "; H::inputLetter(description, 20);
-    cout << "\n\tEdit Flower Date         : "; H::inputDate(date, true);
 
+	H::setcolor(199);H::gotoxy(117,16); cout<<":  "<< GetFlowerId();
+	H::setcolor(199);H::gotoxy(117,18); cout<<":  "; H::inputLetter(name, 15);
+	H::setcolor(199);H::gotoxy(117,20); cout<<":  "; H::inputLetter(type, 15);
+	H::setcolor(199);H::gotoxy(117,22); cout<<":  "; price = stod(H::inputUNumber(strPrice, 6));
+	H::setcolor(199);H::gotoxy(117,24); cout<<":  "; qty = stoi(H::inputUNumber(strQty, 4));
+	H::setcolor(199);H::gotoxy(117,26); cout<<":  "; H::inputLetter(description, 20);
+	H::setcolor(199);H::gotoxy(117,28); cout<<":  "; H::inputDate(date, true);
+	
+	
     SetFlowerName(name);
     SetFlowerType(type);
     SetFlowerPrice(price);
     SetFlowerQty(qty);
     SetFlowerDescription(description);
     SetFlowerDate(date);
+    
+   
+}
+void Product::Header()
+{
+    H::setcolor(1);H::gotoxy(25,15);cout << left << setw(10) << "ID"
+        << setw(20) << "Name"
+        << setw(20) << "Type"
+        << setw(20) << "Price"
+        << setw(15) << "Qty"
+        << setw(28) << "Description"
+        << "Date";
 }
 
 void Product::DisplayFlowerData()
 {
-    cout << left << setw(10) << GetFlowerId()
-         << setw(20) << GetFlowerName()
-         << setw(15) << GetFlowerType()
-         << setw(10) << fixed << setprecision(2) << GetFlowerPrice() << "$"
-         << setw(10) << GetFlowerQty()
-         << setw(30) << GetFlowerDescription()
-         << setw(15) << GetFlowerDate()
-         << endl;
+    H::setcolor(6);cout << left << setw(10) << GetFlowerId()
+        << setw(20) << GetFlowerName()
+        << setw(20) << GetFlowerTpye()
+        << setw(20) << fixed << setprecision(2) << GetFlowerPrice() << "$"
+        << setw(15) << GetFlowerQty()
+        << setw(28) << GetFlowerDescription()
+        << GetFlowerDate();
+        
 }
 
 void Product::EditFlowerData()
 {
+	Product::DesignUpdateInsert();
     int qty;
     double price;
     char type[30], description[100], date[20];
     string strQty, strPrice;
 
-    cout << "\n\tFlower ID                : " << GetFlowerId();
-    cout << "\n\tEdit Flower Name         : " << GetFlowerName();
-    cout << "\n\tEdit Flower Type         : "; H::inputLetter(type, 15);
-    cout << "\n\tEdit Flower Price        : "; price = stod(H::inputUNumber(strPrice, 6));
-    cout << "\n\tEdit Flower Quantity     : "; qty = stoi(H::inputUNumber(strQty, 4));
-    cout << "\n\tEdit Flower Description  : "; H::inputLetter(description, 20);
-    cout << "\n\tEdit Flower Date         : "; H::inputDate(date, true);
+    H::setcolor(199);H::gotoxy(44,14);cout << "Enter Flower ID           : ";
+    H::setcolor(199);H::gotoxy(44,16);cout << "Enter Flower Name         : ";
+    H::setcolor(199);H::gotoxy(44,18);cout << "Enter Flower Type         : "; 
+    H::setcolor(199);H::gotoxy(44,20);cout << "Enter Flower Price        : "; 
+    H::setcolor(199);H::gotoxy(44,22);cout << "Enter Flower Quantity     : "; 
+    H::setcolor(199);H::gotoxy(44,24);cout << "Enter Flower Description  : "; 
+    H::setcolor(199);H::gotoxy(44,26);cout << "Enter Flower Date         : "; 
 
+	H::setcolor(199);H::gotoxy(97,14); cout<<": "<< GetFlowerId();
+	H::setcolor(199);H::gotoxy(97,16); cout<<": " << GetFlowerName();
+	H::setcolor(199);H::gotoxy(97,18); cout<<": "; H::inputLetter(type, 15);
+	H::setcolor(199);H::gotoxy(97,20); cout<<": "; price = stod(H::inputUNumber(strPrice, 6));
+	H::setcolor(199);H::gotoxy(97,22); cout<<": "; qty = stoi(H::inputUNumber(strQty, 4));
+	H::setcolor(199);H::gotoxy(97,24); cout<<": "; H::inputLetter(description, 20);
+	H::setcolor(199);H::gotoxy(97,26); cout<<": "; H::inputDate(date, true);
+	
     SetFlowerType(type);
     SetFlowerPrice(price);
     SetFlowerQty(qty);
     SetFlowerDescription(description);
     SetFlowerDate(date);
-}
-
-void Product::Header()
-{
-    cout << left << setw(10) << "ID"
-         << setw(20) << "Name"
-         << setw(15) << "Type"
-         << setw(10) << "Price"
-         << setw(10) << "Qty"
-         << setw(30) << "Description"
-         << setw(15) << "Date" << endl;
-    cout << string(145, '-') << endl;
 }
 
 // Global objects
@@ -134,6 +225,7 @@ void InputFlowerData()
         return;
     }
 
+
     p.InputFlowerData();
     proF.write((char*)&p, sizeof(p));
     proF.close();
@@ -142,9 +234,8 @@ void InputFlowerData()
 // Function to display all flower records
 void DisplayFlowerData()
 {
-    int y=13;
-
-    H::gotoxy(10,11);Product::Header();
+    int y=19;
+    Product::Header();
     proF.open("Data\\Product.bin", ios::in | ios::binary);
     if (proF.fail())
     {
@@ -152,54 +243,25 @@ void DisplayFlowerData()
         return;
     }
 
-    // Product::Header();
+    Product::Header();
     while (proF.read((char*)&p, sizeof(p)))
     {
-        H::gotoxy(10,y++);p.DisplayFlowerData();
+        H::gotoxy(23,y++);p.DisplayFlowerData();
     }
-
-    proF.close();
-}
-//###############################################################################################
-void SearchFlowerData()
-{
-    int id;
-    string strID;
-    bool found = false;
-
-    cout << "\n\tInput Product ID to Search: ";
-    id = stoi(H::inputUNumber(strID, 5));
-
-    proF.open("Data\\Product.bin", ios::in | ios::binary);
-    if (!proF) {
-        cout << "\tFile not found!" << endl;
-        return;
-    }
-
-    while (proF.read((char*)&p, sizeof(p))) {
-        if (p.GetFlowerId() == id) {
-            Product::Header();
-            p.DisplayFlowerData();
-            found = true;
-            break;
-        }
-    }
-
-    if (!found)
-        cout << "\tProduct ID not found!" << endl;
-
+	Design::Flowercolor();
     proF.close();
 }
 //###############################################################################################
 void UpdateFlowerData()
-{
+{	
+	Design::Search();
     int updateId;
     string strID;
     bool isUpdated = false;
 
-    cout << "\n\tInput Product ID TO Update : ";
-    updateId = stoi(H::inputUNumber(strID, 10));
-
+    H::setcolor(4);H::gotoxy(53,22);cout << "Input Product ID TO Update  ";
+    H::setcolor(4);H::gotoxy(86,22);cout<<":  ";updateId = stoi(H::inputUNumber(strID, 10));
+	H::clearBox(48,20,73,5,1);
     proF.open("Data\\Product.bin", ios::in | ios::out | ios::binary);
     if (!proF) {
         cout << "\tFile not found!" << endl;
@@ -221,7 +283,52 @@ void UpdateFlowerData()
     if (!isUpdated)
         cout << "\tThis Product ID is not found for Update!" << endl;
     else
-        cout << "\tProduct is Updated Successfully!" << endl;
+    {
+        H::drawBoxDoubleLine(76,28,59,1,2);
+		H::setcolor(6);H::gotoxy(82,29);cout<<">>>>>>>> FLOWER  UPDATE SUCCESSFULLY <<<<<<<<";
+		Design::FlowerChoice();
+		Design::Style();
+		Design::Footer();
+    }
+}
+//###############################################################################################
+
+//###############################################################################################
+void SearchFlowerData()
+{
+    int id;
+    string strID;
+    bool found = false;
+
+    H::setcolor(3);H::gotoxy(53,22);cout<<"Input Product ID to Search ";
+    H::setcolor(3);H::gotoxy(86,22);cout<<":  ";id = stoi(H::inputUNumber(strID, 5));
+	H::clearBox(48,20,73,5,1);
+	H::clearBox(19,8,4,30,0);
+	H::clearBox(146,7,4,30,0);
+    proF.open("Data\\Product.bin", ios::in | ios::binary);
+    if (!proF) {
+        cout << "\tFile not found!" << endl;
+        return;
+    }
+    while (proF.read((char*)&p, sizeof(p))) {
+        if (p.GetFlowerId() == id) {
+	    	p.DesignSearchDisplay();
+			H::setcolor(1);H::gotoxy(22,15);Product::Header();
+            H::setcolor(5);H::gotoxy(22,18);p.DisplayFlowerData();
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+        cout << "\tProduct ID not found!" << endl;
+    else
+    {
+		H::drawBoxDoubleLine(76,28,59,1,2);
+		H::setcolor(6);H::gotoxy(82,29);cout<<">>>>>>>> FLOWER  SEARCH SUCCESSFULLY <<<<<<<<";
+		Design::Footer();
+    }
+    proF.close();
 }
 //###############################################################################################
 void DeleteFlowerData()
@@ -230,8 +337,8 @@ void DeleteFlowerData()
     string strID;
     bool isDeleted = false;
 
-    cout << "\t Input ID of Product to Delete : ";
-    id = stoi(H::inputUNumber(strID, 10));
+    H::setcolor(1);H::gotoxy(53,22);cout << "Input ID of Product to Delete  ";
+    H::setcolor(1);H::gotoxy(86,22);cout<<":  ";id = stoi(H::inputUNumber(strID, 10));
 
     proF.open("Data\\Product.bin", ios::in | ios::binary);
     if (proF.fail())
@@ -302,7 +409,7 @@ void ImportFlowerQty() {
                 proF.seekp((int)proF.tellg() - sizeof(p));
                 proF.write((char*)&p, sizeof(p));
 
-                cout << "\t Quantity Imported Successfully!" << endl;
+                cout << "\t? Quantity Imported Successfully!" << endl;
                 break;
             }
         }
@@ -310,7 +417,7 @@ void ImportFlowerQty() {
         proF.close();
 
         if (!found)
-            cout << "\t ID not found! Please try again.\n";
+            cout << "\t? ID not found! Please try again.\n";
 
     } while (!found);
 }
@@ -318,6 +425,7 @@ void ImportFlowerQty() {
 void SortFlowerData() {
     Product products[100];
     int count = 0;
+
 
     proF.open("Data\\Product.bin", ios::in | ios::binary);
     if (!proF) {
@@ -394,8 +502,3 @@ void AlertForLowStock() {}
 //###############################################################################################
 
 #endif
-
-
-
-
-
--- Header/productData.h
+++ Header/productData.h
@@ -1,4 +1,4 @@
-/*
+ /*
             This class use for store
                 - Data Member
                 - Setter or Mutator
@@ -13,107 +13,129 @@
 using namespace ANTHinsyOOP;
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-class Data {
-private:
-    int flower_id;
-    char flower_name[50];
-    char flower_type[30];
-    double flower_price;
-    int flower_qty;
-    char flower_description[100];
-    char flower_date[20];
-public:
-    // Setter
-    void SetFlowerId(int id);
-    void SetFlowerName(const char* name);
-    void SetFlowerType(const char* type);
-    void SetFlowerPrice(double price);
-    void SetFlowerQty(int qty);
-    void SetFlowerDescription(const char* desc);
-    void SetFlowerDate(const char* date);
-    
-    // Getter
-    int GetFlowerId() const;
-    const char* GetFlowerName() const;
-    const char* GetFlowerType() const;
-    double GetFlowerPrice() const;
-    int GetFlowerQty() const;
-    const char* GetFlowerDescription() const;
-    const char* GetFlowerDate() const;
+class Data{
+	private: 
+		//Data Member
+		int flower_id;            		 // Unique ID for each flower Product
+	    char flower_name[50];            // Name of the flower
+	    char flower_type[30];            // Type/category of flower (e.g. Rose, Orchid)
+	    double flower_price;             // Price of the flower
+	    int flower_qty;             	 // Available stock quantity
+	    char flower_color[20];           // Color of the flower
+	    char flower_origin[50];          // Origin country or place
+	    char flower_description[100];    // Short description
+	    char flower_date[20];            // Date added or harvested (dd-mm-yyyy)
+	public:
+		//@@@@ 	Function Prototype 
+		//										setter 
+		void SetFlowerID(int id);
+		void SetFlowerName(const char* name);
+		void SetFlowerType(const char* type);
+		void SetFlowerPrice(double price);
+		void SetFlowerQty(int qty);
+		void SetFlowerColor(const char* color);
+		void SetFlowerOrigin(const char* origin);
+		void SetFlowerDescription(const char* description);
+		void SetFlowerDate(const char* date);
+
+		//											Getter
+		int GetFlowerId() const;
+		const char* GetFlowerName() const;
+		const char* GetFlowerTpye() const;
+		double GetFlowerPrice() const;
+		int GetFlowerQty() const;
+		const char* GetFlowerColor() const;
+		const char* GetFlowerOrigin() const;
+		const char* GetFlowerDescription() const;
+		const char* GetFlowerDate() const;
+		
+		
 };
-////////////////////////////////////////////////////////////////////////////////////////////
-//////////					Body Propotype of Methods in class Data
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-void Data::SetFlowerName(const char* name) 
-{ 
-	strcpy(this->flower_name, name); 
-}
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-void Data::SetFlowerType(const char* type)        
-{ 
-	strcpy(this->flower_type, type); 
-}
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-void Data::SetFlowerPrice(double price)           
-{ 
-	this->flower_price = price; 
-}
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-void Data::SetFlowerQty(int qty)                  
-{ 
-	this->flower_qty = qty; 
-}
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-void Data::SetFlowerDescription(const char* desc) 
-{ 
-	strcpy(this->flower_description, desc); 
-}
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-void Data::SetFlowerDate(const char* date)        
-{ 
-	strcpy(this->flower_date, date); 
-}
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-void Data::SetFlowerId(int id)
+/*      										Body function propotype
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+											Setter or Mutator
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+void Data::SetFlowerID(int id)
 {
 	this->flower_id = id;
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-// Getter
-int Data::GetFlowerId() const 
-{ 
-	return flower_id; 
+void Data::SetFlowerName(const char* name)
+{
+	strcpy(this->flower_name, name);
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-const char* Data::GetFlowerName() const          
-{ 
-	return this->flower_name; 
+void Data::SetFlowerType(const char* type)
+{
+	strcpy(this->flower_type, type);
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-const char* Data::GetFlowerType() const          
-{ 
-	return this->flower_type; 
+void Data::SetFlowerPrice(double price)
+{
+	this->flower_price = price;
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-double Data::GetFlowerPrice() const              
-{ 
-	return this->flower_price; 
+void Data::SetFlowerQty(int qty)
+{
+	this->flower_qty = qty;
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-int Data::GetFlowerQty() const                   
-{ 
+void Data::SetFlowerColor(const char* color)
+{
+	strcpy(this->flower_color, color);
+}
+void Data::SetFlowerOrigin(const char* origin)
+{
+	strcpy(this->flower_origin, origin);
+}
+void Data::SetFlowerDescription(const char* description)
+{
+	strcpy(this->flower_description, description);
+}
+void Data::SetFlowerDate(const char* date)
+{
+	strcpy(this->flower_date, date);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*      										
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+											Getter or Accessor
+	@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+int Data::GetFlowerId() const
+{
+	return this->flower_id;
+}
+
+const char* Data::GetFlowerName() const
+{
+	return this->flower_name;
+}
+const char* Data::GetFlowerTpye() const
+{
+	return this->flower_type;
+}
+double Data::GetFlowerPrice() const
+{
+	return this->flower_price;
+}
+int Data::GetFlowerQty() const
+{
 	return this->flower_qty;
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-const char* Data::GetFlowerDescription() const   
-{ 
-	return this->flower_description; 
+const char* Data::GetFlowerColor() const
+{
+	return this->flower_color;
 }
-//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-const char* Data::GetFlowerDate() const          
-{ 
-	return this->flower_date; 
+const char* Data::GetFlowerOrigin() const
+{
+	return this->flower_origin;
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////
+const char* Data::GetFlowerDescription() const
+{
+	return this->flower_description;
+}
+const char* Data::GetFlowerDate() const
+{
+	return this->flower_date;
+}
+
 
 #endif
